7c5bc65807b43fc7ff4ec70bfe8a72e0a07954d9
claudijo <claudijo.borovic@gmail.com>
2021-10-25 17:32:58 +0300

    Add env vars

--- middleware/session-parser.js
+++ middleware/session-parser.js
@@ -2,10 +2,12 @@ const cookieSession = require('cookie-session')
 
 const sessionParser = cookieSession({
     name: 'session',
-    keys: ['secret', 'keys'],
+    keys: process.env.SESSION_SECRETS.split(','),
 
     // Cookie Options
-    maxAge: 24 * 60 * 60 * 1000 * 365 // 1 year
+    maxAge: 24 * 60 * 60 * 1000 * 365, // 1 year
+    secure: process.env.NODE_ENV !== 'development',
+    httpOnly: true,
 })
 
 module.exports = sessionParser
